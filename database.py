39fa94486841b34ab5692a703011ac06b8964b89
keil4ni <keilanili@gmail.com>
2023-06-18 00:49:57 -0700

    database.py rewritten to apply access/adding methods to our app instead of using sample databases. not too sure how to plug in user input into the function

--- database.py
+++ database.py
@@ -1,15 +1,24 @@
 # attempt to connect python function to mongodb database and add/access items to the database
 
-'''to clean up the code, i'm planning to turn the adding feature and accessing feature into 2 separate functions.
-what i have here right now is just to test with a sample database'''
+'''to clean up the code, i'm planning to turn the adding feature and accessing feature into 2 (or 3?) separate functions.
+i want to apply what i tested to our app since right now i'm just testing with a sample database
+also pymongo isn't importing for some reason Lol i've uninstalled n reinstalled but im not sure what's wrong'''
 
+import pymongo
 from pymongo import MongoClient
 client = MongoClient('mongodb+srv://userhack:ZPO4BzbPYQvRFhf7@hackathoncluster.ljjbuhm.mongodb.net/')
 
 db = client['sample_airbnb']
 collection = db['listingsAndReviews']
 
-# # here, we will try to add data to the database specified above
+def addToDB(**kwargs): # not sure if this will work, but the function will take the user's input of a key value pair and add it to the database collection
+    result = collection.insert_one(kwargs)
+    if result.acknowledged:
+        print('Data was added successfully to the database')
+    else:
+        print('Data could not be added to the database')
+
+# here, we will try to add data to the database specified above
 # data = {
 #     'key1' : 'value1',
 #     'key2' : 'value2',
@@ -20,18 +29,27 @@ collection = db['listingsAndReviews']
 # else:
 #     print('Data could not be added to the database')
 
-# now, we will try to request data from the mongodb and have it be returned to us
-filter_criteria = { # matches conditions you need to retrieve data
-    'bedrooms' : {'$gt' : 2}, # finds airbnbs that have at least 2 bedrooms
-}
 
-results = collection.find(filter_criteria)
+def accessDB(**kwargs): # this function is meant to find the database key value pair that matches the user's input and prints it
+    results = collection.find(kwargs)
+    if results == -1:
+        print('Sorry, the information you provided is not stored in the database')
+    else:
+        print(results)
 
-for document in results:
-    listing = document['listing_url']
-    bedrooms = document['bedrooms']
-    print(f'Listing: {listing}, # of bedrooms: {bedrooms}')
+'''actually might add another function when the user wants to filter information and list specific things from the database but idk how that'll work'''
 
 
+# # now, we will try to request data from the mongodb and have it be returned to us
+# filter_criteria = { # matches conditions you need to retrieve data
+#     'bedrooms' : {'$gt' : 7}, # finds airbnbs that have at least 7 bedrooms
+# }
+#
+# results = collection.find(filter_criteria)
+#
+# for document in results:
+#     listing = document['listing_url']
+#     bedrooms = document['bedrooms']
+#     print(f'Listing: {listing}, # of bedrooms: {bedrooms}') # lists all the airbnbs that have at least 7 bedrooms with their respective urls
 
 client.close() # closes database connection when you're done
