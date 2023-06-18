f9ae5d06a40148c81b387a3070e0c851eab56e6a
JasonVinh Son <jasonson2004@gmail.com>
2023-06-18 14:10:22 -0700

    final overhaul of back-end

--- app.py
+++ app.py
@@ -1,22 +1,27 @@
-# note: coded in Python 3.10.5 x64
-from flask import Flask, render_template, request, jsonify, session
-from flask_pymongo import PyMongo
+from flask import Flask, render_template, request, jsonify, send_from_directory
 import os
 import openai
 from dotenv import load_dotenv
-from pymongo import MongoClient
+import json
 
 load_dotenv()
+
+# Initializing api keys
 openai.api_key = os.getenv("OPENAI_API_KEY")
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'cred.json'
 
+# Initializing app
 app = Flask(__name__)
-app.config['MONGO_URI'] = os.getenv('MONGO_URI')
-mongo = PyMongo(app)
+
+past_msgs = []
 
 @app.route('/')
 def index():
     return render_template('index.html')
+    
+@app.route( '/welcome/<name>' )
+def welcome(name):
+    return render_template('index.html',name=name)
 
 @app.route('/init')
 def initialization():
@@ -26,24 +31,48 @@ def initialization():
 def picksubject_render(lang):
     return render_template('picksubject.html', lang=lang)
 
+'''
+Low priority
+'''
 @app.route('/picksubject/<lang>/modules')
 def modules_render(lang):
     return render_template('modules.html', lang=lang)
 
-@app.route('/picksubject/<lang>/converse')
-def converse_render(lang):
-    return render_template('converse.html', lang=lang)
+'''
+Converse Area
+'''
+@app.route('/picksubject/<lang>/converse', methods=['GET', 'POST'])
+def converse(lang):
+    print(lang)
+    global past_msgs
+     # Create an empty list for past_msgs in the session
+    if request.method == 'POST':
+        audio_data = request.files.get('audio')
+        audio_data.save("voice/currInput.wav")
+        text = listen_for_audio(lang, "voice/currInput.wav")
+        response, messages = conversateGPT(lang, text)
+        past_msgs.append(messages)
+        print(past_msgs)
+        save_entire_sentence(lang, response)
+        return jsonify({'response': response, 'audio_url': "/get_audio/sentence.wav"})
+    else:
+        # Render the page on a GET request.
+        return render_template('converse.html')
 
-@app.route( '/welcome/<name>' )
-def welcome(name):
-    return render_template('index.html',name=name)
 
+@app.route('/get_audio/<path:filename>', methods=['GET'])
+def get_audio(filename):
+    return send_from_directory('voice', filename)
+
+'''
+Practice Area
+'''
 @app.route( '/picksubject/<lang>/practice' )
 def practice_page_render(lang):
-    return render_template('practice.html', lang=lang)
+    return render_template('practice.html', syllables_dict=(requestGPT(lang,3)))
 
-
-def requestGPT(sentence:str, method:int): 
+# Finished
+def requestGPT(sentence:str, method:int):
     """
     Args:
         sentence: the sentence to be processed by GPT
@@ -107,150 +136,165 @@ Here's your first one (REMEMBER TO ONLY REPLY IN THE ARRAY):
             )
             res = response['choices'][0]['message']['content']
             return res
+        case 2:
+            messages = [{"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": """Please return a translation to English of what I say, and nothing else.:
+            """},
+            {"role": "user", "content": sentence}]
+            response = openai.ChatCompletion.create(
+                model="gpt-3.5-turbo",
+                messages=messages,
+                max_tokens=250,
+            )
+            res = response['choices'][0]['message']['content']
+            return res
+        case 3:
+            messages = [{"role": "system", "content": "Please create curriculum of 10 vocabulary words along with helpful tips of pronunciation as their values for the language that I specify that is easy-intermediate. Do not say anything else except for the dict. Please format in a Python dictinary format. This is made for those who just want to learn words. Should be in format {'word': 'helpful tip on how to pronounce'}"},
+            {"role": "user", "content": sentence}]
+            response = openai.ChatCompletion.create(
+                model="gpt-3.5-turbo",
+                messages=messages,
+                max_tokens=250,
+            )
+
+            return res
+        
     return "GPT is not accessible at this time."
 
-def conversateGPT( lang: str, sentence: str ) -> str:
-        messages = [
-            {"role": "system", "content": "You are a helpful assistant."},
-            {"role": "user", "content": """
-You are an avid individual who loves learning languages -- you have knowledge about every language in existence, except for the extinct ones of course. If I do not respond in a correct language, or if you just don't understand my response because it's too vague, respond with 'What did you say?' in the respective language. Your job is to practice with me to speak. Do not start until I say 'Hello' to you -- when I say hello, begin a beginner-level conversation from easy terms in a language that I prefer, then gradually (very slowly) increase in difficulty. For this conversation, let's start with {}""".format(lang),
-                },
-                {"role": "user", "content": sentence}
-            ]
-        response = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo",
-            messages=messages,
-            max_tokens=250,
-        )
-        res = response['choices'][0]['message']['content']
-        return res
+'''
+TODO: Need to retain past messages
+'''
+def conversateGPT(lang: str, sentence: str, past_msgs=[]) -> str:
+    stop_sentences: dict = {
+        "korean": ["초기화", "하지마"],
+        "english": "Reset",
+        "japanese": "リセット",
+        "mandarin": "重置",
+        "spanish": "Reiniciar"
+    }
+    # Define the system message and initial user message
+    system_message = {"role": "system", "content": "You are a helpful assistant."}
+    user_message_init = {
+        "role": "user",
+        "content": f"""
+        You are an avid individual who loves learning languages -- you have knowledge about every language in existence, except for the extinct ones of course. If I do not respond in a correct language, or if you just don't understand my response because it's too vague, respond with 'What did you say?' in the respective language. Your job is to practice with me to speak. Do not start until I say 'Hello' to you -- when I say hello, begin a beginner-level conversation from easy terms in a language that I prefer, then gradually (very slowly) increase in difficulty. You'll return only the sentence without translation and nothing else. Please act naturally, as if you're a friend. For this conversation, let's start with {lang}"""
+    }
+    user_message = {"role": "user", "content": sentence}
 
-def listen_for_audio( lang: str, matching: str ) -> bool:
+    # Edge case
+    if sentence.lower() in stop_sentences.get(lang, []):
+        if past_msgs is not None:
+            past_msgs.clear()
+        return "Conversation reset."
+    
+    # Prepare the messages
+    if len(past_msgs) > 0:
+        messages = [system_message, user_message_init, user_message] + past_msgs
+    else:
+        messages = [system_message, user_message_init, user_message]
+
+    # Call the API
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=messages,
+        max_tokens=250,
+    )
+
+    # Get the response
+    res = response['choices'][0]['message']['content']
+    messages.append({"role": "assistant", "content": res})
+
+    past_msgs.append(({"role": "assistant", "content": res}))
+    # Update the messages in the database
+    return res, past_msgs
+
+# Finished
+def listen_for_audio(lang: str, audio_file_path: str) -> str:
     '''
     Args:
         lang: language to listen for
-        matching: the matching string to compare to
+        audio_file_path: path to the audio file to be processed
     Returns:
-        true if the matching string is said, false otherwise
-
-    example call
-    listen_for_audio("Korean", "안녕")
+        recognized text from the audio file
     '''
     import speech_recognition as sr
-    legend= {
-    "English": "en_GB",
-    "Korean": "ko_KR", 
-    "Japanese": "ja_JP",
-    "Mandarin": "zh_CN",
-    "Spanish": "es_ES"
+    legend = {
+        "english": "en-GB",
+        "korean": "ko-KR", 
+        "japanese": "ja-JP",
+        "mandarin": "zh-CN",
+        "spanish": "es-ES"
     }
-    lang=lang.capitalize()
+    # Ensure that the provided language is supported, if not default to English
+    if lang not in legend:
+        print(f'Unsupported language "{lang}". Defaulting to English.')
+        lang = "english"
+
     r = sr.Recognizer()
-    with sr.Microphone() as source:
-        print("Listening...")
-        audio = r.listen(source, phrase_time_limit=7.5)  # Set a time limit for each phrase
-        print("Stopped listening.")
+
+    with sr.AudioFile(audio_file_path) as source:
+        print("Processing audio file...")
+        audio = r.record(source)  # Read the entire audio file
+        print("Finished processing.")
 
     try:
-        text = r.recognize_google(audio, language=legend[lang])  # Use 'zh-CN' for Mandarin
-        print("{}".format(text))
-    except:
-        raise "Didn't understand this."
-    else: return text
-    
-def setup_curriculum(language: str, curriculum: dict, level: int) -> set:
-    from random import choice
-    """Sets up the curriculum for the given language.
-    
-    Example usage:
-    setup_curriculum("Korean", dataset, 2)
-    -> set of 10 words that are at level 2 or below in the Korean curriculum
+        text = r.recognize_google(audio, language=legend[lang])
+        print(f"Transcribed Text: {text}")
+    except sr.UnknownValueError:
+        print("Google Speech Recognition could not understand audio")
+        text = None
+    except sr.RequestError as e:
+        print(f"Could not request results from Google Speech Recognition service; {e}")
+        text = None
 
-    Args:
-        language (str): language of curriculum
-        curriculum (dict): list of words
+    return text
 
-    Returns:
-        set of words that will be depicted in the lesson, always will be 10 words
-    """
-    res = []
-    for i in range(level + 1):
-        for j in range(i + 1):
-            curriculum_i = curriculum.get(j)
-            if curriculum_i:
-                res.extend(curriculum_i)
-    return [choice(res) for _ in range(10)]
-
-import pygame
-import os
+# Finished
 from google.cloud import texttospeech
-
-def speak_syllable_by_syllable(lang: str, sentence: str) -> None:
+def save_entire_sentence(lang: str, sentence: str) -> None:
+    lang=lang.lower()
     legend= {
-    "English": "en_GB",
-    "Korean": "ko_KR",
-    "Japanese": "ja_JP",
-    "Mandarin": "zh_CN",
-    "Spanish": "es_ES"
+    "english": "en-GB",
+    "korean": "ko-KR",
+    "japanese": "ja-JP",
+    "mandarin": "zh-CN",
+    "spanish": "es-ES"
     }
-    
-    arr: list = requestGPT(sentence, 1)
-    print(arr)
-    
     client = texttospeech.TextToSpeechClient()
     voice = texttospeech.VoiceSelectionParams(
-    language_code=legend[lang.capitalize()], 
+    language_code=legend[lang], 
     ssml_gender=texttospeech.SsmlVoiceGender.FEMALE)
 
     audio_config = texttospeech.AudioConfig(
-    audio_encoding=texttospeech.AudioEncoding.MP3)
-
-    os.makedirs("syllables", exist_ok=True)
-
-    i = 0  # Initialize word counter
-    for word in arr:  # Removed i from enumerate
-        j = 0  # Initialize syllable counter for each word
-        word_has_valid_syllable = False  # Keep track if word has at least one valid syllable
-        for syllable in word:  # Removed j from enumerate
-            syllable = syllable.strip()  # Remove leading and trailing whitespace
-            # Skip if syllable is empty, whitespace or punctuation
-            if not syllable or syllable in {',', '[', ']', ' '}:
-                continue
-            # Synthesize the speech
-            response = client.synthesize_speech(
-                input=texttospeech.SynthesisInput(text=syllable),
-                voice=voice,
-                audio_config=audio_config)
-
-            # Write the response to an MP3 file
-            filename = f"syllables/word{i}_syllable{j}.mp3"
-            with open(filename, "wb") as out:
-                out.write(response.audio_content)
-
-            j += 1  # Increment syllable counter only after a valid syllable is processed
-            word_has_valid_syllable = True  # Set flag to True because a valid syllable was found
-
-        if word_has_valid_syllable:  # Only increment word counter if word had at least one valid syllable
-            i += 1
-
-    # Initialize pygame
-    pygame.mixer.init()
-
-    # Get the list of all files in the directory
-    files = os.listdir("syllables")
-
-    # Play each file one by one
-    for file in sorted(files):
-        filename = f"syllables/{file}"
-        pygame.mixer.music.load(filename)
-        pygame.mixer.music.play()
-        while pygame.mixer.music.get_busy() == True:
-            continue  
+    audio_encoding=texttospeech.AudioEncoding.LINEAR16) 
+    os.makedirs("voice", exist_ok=True)
+    response = client.synthesize_speech(
+        input=texttospeech.SynthesisInput(text=sentence),
+        voice=voice,
+        audio_config=audio_config)
     
-    # End, remove all syllables
-    for file in files:
-        os.remove(f"syllables/{file}")
+    filename = f"voice/sentence.wav"
+    with open(filename, "wb") as out:
+        out.write(response.audio_content)
+    
+    # Translating to English
+    sentence = requestGPT(sentence, 2)
+    voice = texttospeech.VoiceSelectionParams(
+    language_code=legend["english"], 
+    ssml_gender=texttospeech.SsmlVoiceGender.FEMALE)
+    response = client.synthesize_speech(
+        input=texttospeech.SynthesisInput(text=sentence),
+        voice=voice,
+        audio_config=audio_config)
+    filename = f"voice/sentenceTranslation.wav"
+    with open(filename, "wb") as out:
+        out.write(response.audio_content)
+    
+'''
+Syllables
+'''
+def get_syllables(lang: str, sentence: str) -> list:
+    requestGPT(sentence,0)
 
-if __name__ == '__main__':
-    speak_syllable_by_syllable("Korean", "안녕하세요")
-  #  app.run(port=5000, debug=True)
+if __name__ == "__main__":
+    app.run()
